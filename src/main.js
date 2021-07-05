a18c75234ec34bc45b78a8a54d5686e18f51f876
German Vargas <german.vargas@cedi.com.ar>
2021-07-05 10:12:21 -0300

    descarga

--- src/main.js
+++ src/main.js
@@ -27,4 +27,31 @@ app.on('ready', () => {
   win.setMenu(null);
   win.maximize();
   win.loadURL(url, { extraHeaders: 'pragma: no-cache\n' });
+
+  win.webContents.on('did-finish-load', () => {
+
+    win.webContents.session.on('will-download', (event, item, webContents) => {
+      // Set the save path, making Electron not to prompt a save dialog.
+      item.setSavePath('/tmp/save.ica');
+
+      item.on('updated', (event, state) => {
+        if (state === 'interrupted') {
+          console.log('Download is interrupted but can be resumed')
+        } else if (state === 'progressing') {
+          if (item.isPaused()) {
+            console.log('Download is paused')
+          } else {
+            console.log(`Received bytes: ${item.getReceivedBytes()}`)
+          }
+        }
+      });
+      item.once('done', (event, state) => {
+        if (state === 'completed') {
+          console.log('Download successfully')
+        } else {
+          console.log(`Download failed: ${state}`)
+        }
+      });
+    });
+  });
 });
